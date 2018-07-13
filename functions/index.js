0c7df77fd4cb23ce2166d3b8ee8ada4cf5cfe6e0
mht-rentaro-matsushita <r.matsushita@mht-code.com>
2018-07-13 15:25:25 +0900

    通知のAPI追加

--- functions/index.js
+++ functions/index.js
@@ -47,3 +47,13 @@ exports.init = functions.https.onRequest((req, res) => {
    });
 });
 
+exports.notification = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+  const original = req.query.uuid;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  admin.database().ref('/user/' + original + '/videos').on('value', (snapshot, prevChildKey) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.json(snapshot.val());
+  });
+});
+
